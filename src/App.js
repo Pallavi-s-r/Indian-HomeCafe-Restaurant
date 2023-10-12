c19823b7e4510315e381dc4a0ba97f6109c2b4ee
Pallavi Singh <78789162+Pallavi-s-r@users.noreply.github.com>
2023-10-12 20:17:52 +0530

    added modifications in findus done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
- import './App.css';
- import Navbar from './components/Navbar/Navbar';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import AboutUs from './container/AboutUs/Aboutus';
-import Footer from './container/Footer/Footer';
- import Gallery from './container/Gallery/Gallery';
- import Header from './container/Header/Header';
-  import Intro from './container/Intro/Intro';
+
+import Gallery from './container/Gallery/Gallery';
+import Header from './container/Header/Header';
+import Intro from './container/Intro/Intro';
 
 import Chef from './container/Chef/Chef';
 import Findus from './container/Findus/Findus';
@@ -16,16 +16,15 @@ import SpecialMenu from './container/SpecialMenu/SpecialMenu';
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Header/>
-      <AboutUs/>
-      <SpecialMenu/>
-      <Chef/>
-      <Intro/>
-     
-<Gallery />
-    <Findus />
-    <Footer/>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Gallery />
+      <Findus />
+
     </div>
   )
 }
--- src/container/Findus/Findus.jsx
+++ src/container/Findus/Findus.jsx
@@ -13,12 +13,18 @@ return (
         <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
         <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
         <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+        <p className="p__opensans">Contact us : +91234567890</p>
+        <p className="p__opensans">Mail at : indianhomecafe@gmail.com</p>
       </div>
       <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Visit Us</button>
     </div>
 
-    <div className="app__wrapper_img">
+    <div className="app__wrapper_img" style={{display:'flex', flexDirection:'column'}}>
       <img src={images.findus} alt="finus_img" />
+       <div className="app__wrapper-content">
+       <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>We take reservations for Party's & Wedding's Services .  </p>
+       <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Book Now - We are ready to serve with best Quality    </p>
+      </div>
     </div>
   </div>
 )
